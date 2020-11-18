f8b181b2656b6e93820b29eec5162c0dc9f659f4
Yannick Denker <YDenkerwork@gmail.com>
2020-11-18 23:41:39 +0100

    Exclude Node js + Vertex & Fragment shader
    I've decided not to use a Node express server to display my results but instead a live server addon for vs code.
    I've created very simple vertex and fragment shader and use them to display a red triangle on screen.
    Note to self: do not name any variable 'location' !

---
+++ Uebung03/main.js
@@ -0,0 +1,64 @@
+var canvas = document.getElementById('canvas');
+var gl = canvas.getContext('webgl');
+
+if(!gl){
+    throw new Error('WebGL not supported');
+}
+
+gl.depthFunc(gl.LEQUAL); //Tiefentest einschalten
+gl.enable(gl.DEPTH_TEST);
+
+gl.clearColor(0.3,0.9,0.3,1.0); //Color Buffer: Farb- und Alphawerte (RGBA)
+
+gl.clearDepth(1.0); //Depth Buffer: speichert Tiefe z für Pixel
+
+gl.viewport(0,0,canvas.clientWidth, canvas.clientHeight);
+
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); //Setzen des Tiefenbits
+
+gl.colorMask(true,true,true,true); //Puffer vor Schreiben maskieren
+gl.depthMask(true);
+
+
+var vertexData = [
+    0,1,0,
+    1,-1,0,
+    -1,-1,0
+];
+
+var buffer = gl.createBuffer(); // Buffer ertellen
+gl.bindBuffer(gl.ARRAY_BUFFER,buffer); // vertexdata in Buffer laden
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+var vertexShader = gl.createShader(gl.VERTEX_SHADER); // Vertex shader erzeugen
+gl.shaderSource(vertexShader,`
+attribute vec3 position;
+void main() {
+    gl_Position = vec4(position, 1);
+}
+`);
+gl.compileShader(vertexShader);
+
+var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); // Fragment shader erzeugen
+gl.shaderSource(fragmentShader, `
+void main(){
+    gl_FragColor = vec4(1,0,0,1);
+}
+`);
+gl.compileShader(fragmentShader);
+
+var program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+var posAttribLocation = gl.getAttribLocation(program, `position`);
+gl.enableVertexAttribArray(posAttribLocation);
+gl.vertexAttribPointer(posAttribLocation, 3, gl.FLOAT, false, 0,0);
+
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+gl.disableVertexAttribArray(posAttribLocation);
+gl.deleteBuffer(buffer);
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const directory = 'Doodles'
-
-app.use(express.static(directory+'/public'));
-
-app.listen(port);
